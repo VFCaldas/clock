fc88a2d5feb13ab84275385e325db919d98232b7
Victor Caldas <victor.solid3@gmail.com>
2020-02-23 14:11:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let number = 50; /* This is a integer and positive number! */
+let DecNumber = 9.99; /* This is a decimal number */
+let NegNumber = -39.99; /* This is a negative and a decimal number */
+/* If you see in console, you will notice that all of those number are the of type number */
